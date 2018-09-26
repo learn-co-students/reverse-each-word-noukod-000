e671cb39901dcccb8b5ca98e1ff2128071d3af23
Saint-Jean Germila <germila@noukod.com>
2018-09-26 18:43:14 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,7 @@
-def reverse_each_word(reverse_each_word)
-      reverse_each_word("olleH ,ereht dna woh era ?uoy")
+def reverse_each_word sentence
+      arraysentence sentence.split(' ')
+
+      reversedsentence = arraysentence.collect { |word| word.reverse}
+
+      reversesentence.join(' ')
     end
-      reverse_each_word("iH ,niaga tsuj gnikam erus s'ti !desrever")
-    end
-      reverse_each_word("Verifying that collect is being called.")
-    end
-      reverse_each_word.collect do |reverse_each_word|
-        puts "#{reverse_each_word}"
-     end
-   end
