4470b9edcb532035aa7db84cd020b246513735cf
Saint-Jean Germila <germila@noukod.com>
2018-09-26 18:36:07 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word(reverse_each_word)
+      reverse_each_word("olleH ,ereht dna woh era ?uoy")
+    end
+      reverse_each_word("iH ,niaga tsuj gnikam erus s'ti !desrever")
+    end
+      reverse_each_word("Verifying that collect is being called.")
+    end
+      reverse_each_word.collect do |reverse_each_word|
+        puts "#{reverse_each_word}"
+     end
+   end
